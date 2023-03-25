e80b522d04b6a10d5c4f71e3051edf1ede4ec2a2
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-03-25 05:53:08 +0000

    add

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
+    @user = current_user
   end
 
   def show
@@ -7,4 +9,11 @@ class UsersController < ApplicationController
 
   def edit
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
 end
