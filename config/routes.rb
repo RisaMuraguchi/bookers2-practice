edc87249987c7a6d269a349edad7e1a11b98e1ac
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-04-03 09:39:50 +0000

    コメント機能モデル/コントローラ追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :books
+  resources :books do
+    resources :book_comments, only:[:create, :destroy]
+  end
   resources :users
   root to: "homes#top"
   get "home/about" => "homes#about", as: "about"
