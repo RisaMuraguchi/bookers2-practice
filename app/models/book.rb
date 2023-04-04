f4c37ec15ec00b17916e3e7813d29ecb6c3fc1a4
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-04-04 01:11:54 +0000

    いいね機能追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,4 +5,10 @@ class Book < ApplicationRecord
 
   belongs_to :user
   has_many :book_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :books do
     resources :book_comments, only:[:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
   end
   resources :users
   root to: "homes#top"
